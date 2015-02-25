67c8b7c334ce318ab5f4eb2cde16e1166c278156
Spiria <yoon0431@gmail.com>
2015-02-25 16:01:52 +0900

    Added COMPONENT_DTLS (Commit Test)

--- inc/opendefs.h
+++ inc/opendefs.h
@@ -146,17 +146,18 @@ enum {
    COMPONENT_OPENTCP                   = 0x17,
    COMPONENT_OPENUDP                   = 0x18,
    COMPONENT_OPENCOAP                  = 0x19,
+   COMPONENT_DTLS                      = 0x1a,
    // applications
-   COMPONENT_C6T                       = 0x1a,
-   COMPONENT_CEXAMPLE                  = 0x1b,
-   COMPONENT_CINFO                     = 0x1c,
-   COMPONENT_CLEDS                     = 0x1d,
-   COMPONENT_CSTORM                    = 0x1e,
-   COMPONENT_CWELLKNOWN                = 0x1f,
-   COMPONENT_TECHO                     = 0x20,
-   COMPONENT_TOHLONE                   = 0x21,
-   COMPONENT_UECHO                     = 0x22,
-   COMPONENT_RRT                       = 0x23,
+   COMPONENT_C6T                       = 0x1b,
+   COMPONENT_CEXAMPLE                  = 0x1c,
+   COMPONENT_CINFO                     = 0x1d,
+   COMPONENT_CLEDS                     = 0x1e,
+   COMPONENT_CSTORM                    = 0x1f,
+   COMPONENT_CWELLKNOWN                = 0x20,
+   COMPONENT_TECHO                     = 0x21,
+   COMPONENT_TOHLONE                   = 0x22,
+   COMPONENT_UECHO                     = 0x23,
+   COMPONENT_RRT                       = 0x24,
 };
 
 /**
